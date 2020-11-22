33e291e0112b314b7def7cdc0024e08e96d8d52b
Andrea Payne <anoelpayne@gmail.com>
2020-11-22 16:32:44 -0600

    final

---
+++ Numeric-Types/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Numeric_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input rectangle length: ");
+            string recLength = Console.ReadLine();
+            double length = double.Parse(recLength);
+            Console.WriteLine("Input rectangle width: ");
+            string recWidth = Console.ReadLine();
+            double width = double.Parse(recWidth);
+            Console.WriteLine("The area of the rectangle is: " + length * width);
+
+        }
+    }
+}
---
+++ Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alice = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing" +
+                " to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, " +
+                "'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
+            Console.WriteLine("Search term: ");
+            string input = Console.ReadLine();
+            int index = alice.ToLower().IndexOf(input.ToLower());
+            if (index > -1)  
+            {
+                Console.WriteLine(input + " is located at index " + index + ". And has a length of " + input.Length);
+                string newAlice = alice.Substring(0, index - 1) + alice.Substring(index + input.Length);
+                Console.WriteLine(newAlice);
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
